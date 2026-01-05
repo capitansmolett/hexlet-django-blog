346fa6469b66bfcd664721a8c85e216c82487255
777 <kiselevvsvs@gmail.com>
2026-01-05 18:48:13 +0300

    add base.html

--- hexlet-django-blog/hexlet_django_blog/article/views.py
+++ hexlet-django-blog/hexlet_django_blog/article/views.py
@@ -1,5 +1,11 @@
 from django.http import HttpResponse
-
+from django.shortcuts import render
 
 def index(request):
-    return HttpResponse("article")
+    return render(
+        request,
+        "index.html",
+        context={
+            "who": "World",
+        },
+    )
